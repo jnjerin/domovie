b233152abf0016a229bb95e6f30e5fb91198954c
jnjerin <njerijoan24@gmail.com>
2024-08-10 00:00:15 +0300

    divs for menu item

--- src/Components/MenuItem.jsx
+++ src/Components/MenuItem.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function MenuItem() {
   return (
-    <div>MenuItem</div>
+    <div>
+        <div className="">
+            
+        </div>
+        <div>
+            
+        </div>
+    </div>
   )
 }
