84a347f45195e98b3f75896a44fa7ee82ac1cb87
jnjerin <njerijoan24@gmail.com>
2024-08-13 02:20:32 +0300

    use mounted for loading

--- src/Components/DarkModeSwitch.jsx
+++ src/Components/DarkModeSwitch.jsx
@@ -1,19 +1,26 @@
 'use client'
 import {MdLightMode, MdDarkMode} from "react-icons/md"
 import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
 
 export default function DarkModeSwitch() {
   const {systemTheme, theme, setTheme} = useTheme()
+  const [mounted, setMounted] = useState(false)
   const currentTheme = theme === "system" ? systemTheme : theme
+  useEffect(() => setMounted(true), [])
   return (
-    <div>{currentTheme === 'dark' ? (
-        <MdLightMode 
-        onClick={() => setTheme('light')}
-            className='txt-xl cursor-pointer hover:text-green-800'  />
+    <div>
+        {mounted && (currentTheme === 'dark' ? (
+            <MdLightMode 
+                onClick={() => setTheme('light')}
+                className='txt-xl cursor-pointer hover:text-green-800'  
+            />
         ) : (
-        <MdDarkMode
-        onClick={() => setTheme('dark')} 
-            className='txt-xl cursor-pointer hover:text-green-500' />)}</div>
+            <MdDarkMode
+                onClick={() => setTheme('dark')} 
+                className='txt-xl cursor-pointer hover:text-green-500' 
+            />
+        ))}
+    </div>
   )
 }
-
