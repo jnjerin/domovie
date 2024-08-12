59a34aa6151f24e5c2e8dcff6d2b329184761cde
jnjerin <njerijoan24@gmail.com>
2024-08-12 15:46:57 +0300

    styled header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,25 @@
 import MenuItem from "./MenuItem"
 import {AiFillHome} from "react-icons/ai"
 import {BsFillInfoCircleFill} from "react-icons/bs"
+import Link from "next/link"
 
 
 export default function Header() {
   return (
-    <div>
-        <div className="">
+    <div className="flex justify-between items-center p-6 max-w-6xl mx-auto">
+        <div className="flex gap-4 mt-2">
             <MenuItem title="Home" address="/" Icon={AiFillHome} />
-            <MenuItem title="Home" address="/about" Icon={BsFillInfoCircleFill} />
+            <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
         </div>
-        <div className="">
-
-        </div>
+        <Link href={'/'} className="">
+            <span className="text-2xl 
+                           bg-green-500 
+                             rounded-lg 
+                             px-2 py-2 
+                             font-bold">
+            DoMovie
+            </span>
+        </Link>
     </div>
 
     
--- src/Components/MenuItem.jsx
+++ src/Components/MenuItem.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Link from 'next/link'
 
-export default function MenuItem(title, address, Icon) {
+export default function MenuItem({title, address, Icon}) {
   return (
-    <Link>
-        <Icon />
-        <p>{title}</p>
+    <Link href={address} className='hover:text-green-600'>
+        <Icon className='text-2xl sm:hidden' />
+        <p className='font-bold uppercase hidden sm:inline text-sm'>{title}</p>
     </Link>
   )
 }
