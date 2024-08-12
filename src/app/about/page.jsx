303831f7e5d6c2e4a4edf1e19a76b02b36caa155
jnjerin <njerijoan24@gmail.com>
2024-08-13 02:40:36 +0300

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-green-600'>About</h1>
+        <p>
+            This is a movie recommendation app. It uses TMDB API to fetch movie data. 
+            It uses Next.js and Tailwind CSS.
+        </p>
+        <p>
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Rhoncus arcu neque fermentum quis ut ornare lorem. Varius tempor dictum maximus himenaeos maecenas primis natoque fames. Nostra aenean quisque faucibus, fermentum facilisis senectus. Class nibh vitae vehicula per habitasse elit ornare. 
+            Efficitur maximus senectus nisl senectus himenaeos dictumst duis. 
+            Nibh nunc sem curabitur pretium volutpat morbi neque dolor. Habitant a urna quam auctor libero fringilla tellus vitae gravida. Pretium nulla orci volutpat primis inceptos; ridiculus pellentesque.
+        </p>
+
+        <p>
+            Dolor laoreet quam dapibus ridiculus curae posuere. 
+            Elit habitant dui libero turpis suspendisse; pharetra class! Nascetur efficitur cursus efficitur turpis litora per, 
+            himenaeos dignissim congue. Nam natoque aptent luctus elementum, lobortis dictumst metus metus tristique. Sodales at sit elit platea justo sodales justo magnis massa? Justo ante orci nisi porta consequat lobortis commodo nostra aliquet. Taciti natoque litora parturient purus diam cursus. Non ante ultricies porta pretium habitasse, hac enim habitant. Varius iaculis natoque pulvinar iaculis urna. Ultricies iaculis aliquam orci dignissim natoque mollis.
+        </p>
+    </div>
+  )
+}
+   
