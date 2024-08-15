39f392fbd8f8f756b7d07724573e43c1ebeee705
jnjerin <njerijoan24@gmail.com>
2024-08-15 11:37:59 +0300

    enable cards to fetch specific movie data and display

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
+import { FiThumbsUp } from "react-icons/fi"
 
 export default function Card({ result }) {
   return (
@@ -13,6 +14,17 @@ export default function Card({ result }) {
                 height={300}
                 className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-300'
             ></Image>
+                <div className='p-2'>
+                    <p className="line-clamp-2 text-md">{result.overview}</p>
+                    <h2 className="text-lg font-bold truncate">
+                        {result.title || result.name}
+                    </h2>
+                    <p className="flex items-center text-gray-300 text-sm">
+                        {result.media_type && `${result.media_type} •`}{" "}
+                        {result.release_date || result.first_air_date} •
+                        <FiThumbsUp className="h-5 mr-1 ml-3" /> {result.vote_count}           
+                    </p>
+                </div>  
         </Link>
     </div>
   )
