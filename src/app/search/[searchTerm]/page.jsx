3daf5b12b56ef4035e06e120365e60b358bdb3f3
jnjerin <njerijoan24@gmail.com>
2024-10-29 10:12:31 +0300

    Update import path to solve errors

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,5 +1,4 @@
-// import Results from '@/components/Results.jsx';
-import Results from '../../components/Results.jsx'
+import Results from '@/Components/Results.jsx';
 
 export default async function SearchPage({ params }) {
   const seachTerm = params.searchTerm;
