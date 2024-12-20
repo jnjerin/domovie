68f9d64b0f26f470641dc103f644fc5ec4c5cf5a
Joan Ndegwa <njerijoan24@gmail.com>
2024-12-20 19:08:43 +0300

    add suspense boundary for proper client-side rendering on deployment

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Results from "@/Components/Results.jsx";
+import { Suspense } from "react";
 const API_KEY = process.env.API_KEY;
-
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending'
-  const  res = await fetch(
+  const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
     }?api_key=${API_KEY}&language=en-US&page=1`,
@@ -15,11 +15,12 @@ export default async function Home({ searchParams }) {
     throw new Error('Failed to fetch data')
   }
   const results = data.results
-  console.log(results)
 
   return (
-    <div>
-      <Results results={results} />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <Results results={results} />
+      </div>
+    </Suspense>
   )
 }
